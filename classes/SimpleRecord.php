24e6d9e45658ae13892008ace39f0dfe48c7f35e
Bob Fanger <bfanger@gmail.com>
2011-09-21 12:46:13 +0200

    SimpleRecord now based on the (rewritten) abstract Record class.

--- classes/SimpleRecord.php
+++ classes/SimpleRecord.php
@@ -5,15 +5,7 @@
  * @package Record
  */
 namespace SledgeHammer;
-class SimpleRecord extends Object {
-
-	private $_model;
-	private $_state = 'unconstructed';
-	private $_repository = '__not_set__';
-
-	function __construct() {
-		$this->_state = 'constructed';
-	}
+class SimpleRecord extends Record {
 
 	/**
 	 *
@@ -40,14 +32,14 @@ class SimpleRecord extends Object {
 
 	/**
 	 *
-	 * @param string $model
+	 * @param string $model (required)
 	 * @param array $values
 	 * @param array $options array(
 	 *   'repository' => (string) "default"
 	 * )
 	 * @return SimpleRecord
 	 */
-	static function create($model, $values = array(), $options = array()) {
+	static function create($model = null, $values = array(), $options = array()) {
 		$repository = value($options['repository']) ?: 'default';
 		$repo = getRepository($repository);
 		$instance = $repo->create($model, $values);
@@ -60,60 +52,12 @@ class SimpleRecord extends Object {
 		throw new \Exception('Model "'.$model.'" isn\'t configured as SimpleRecord');
 	}
 
-	function __get($property) {
-		if ($this->_state == 'deleted') {
-			notice('A deleted Record has no properties');
-			return null;
-		} else {
-			return parent::__get($property);
-		}
-	}
-
 	public function __set($property, $value) {
-		switch ($this->_state) {
-
-			case 'constructed':
-				$this->$property = $value;
-				break;
-
-			case 'deleted':
-				notice('A deleted Record has no properties');
-				break;
-
-			default:
-				return parent::__set($property, $value);
+		if ($this->_state == 'constructed') {
+			$this->$property = $value;
+		} else {
+			return parent::__set($property, $value);
 		}
-	}
-
-	function save() {
-		if ($this->_state == 'deleted') {
-			throw new \Exception(__CLASS__.'->save() not allowed on deleted objects');
-		}
-		$repo = getRepository($this->_repository);
-		$repo->save($this->_model, $this);
-	}
-
-	function delete() {
-		$repo = getRepository($this->_repository);
-		$repo->remove($this->_model, $this);
-		// unset properties
-		$propertyWhitelist = array_keys(get_class_vars(__CLASS__));
-		foreach (get_object_vars($this) as $property => $value) {
-			if (in_array($property, $propertyWhitelist) == false) {
-				unset($this->$property);
-			}
-		}
-		$this->_state = 'deleted';
-	}
-
-
-	/**
-	 *
-	 * @return array
-	 */
-	function getChanges() {
-		$repo = getRepository($this->_repository);
-		return $repo->diff($this->_model, $this);
 	}
 }
 ?>
