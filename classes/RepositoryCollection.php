d1d18accb9c13b0ac4da3eb03577780d97fe3fb8
Bob Fanger <bfanger@gmail.com>
2012-05-07 02:03:36 +0200

    Improved performance of RepositoryCollection->orderBy() operations

--- classes/RepositoryCollection.php
+++ classes/RepositoryCollection.php
@@ -78,6 +78,20 @@ class RepositoryCollection extends Collection {
 		return parent::take($length);
 	}
 
+	function orderBy($path, $method = SORT_REGULAR) {
+		if ($this->isConverted === false && isset($this->mapping[$path]) && $this->data instanceof Collection) {
+			return new RepositoryCollection($this->data->orderBy($this->mapping[$path], $method), $this->model, $this->repository, $this->mapping);
+		}
+		return parent::orderBy($path, $method);
+	}
+
+	function orderByDescending($path, $method = SORT_REGULAR) {
+		if ($this->isConverted === false && isset($this->mapping[$path]) && $this->data instanceof Collection) {
+			return new RepositoryCollection($this->data->orderByDescending($this->mapping[$path], $method), $this->model, $this->repository, $this->mapping);
+		}
+		return parent::orderBy($path, $method);
+	}
+
 	/**
 	 * Return the number of elements in the collection.
 	 * count($collection)
